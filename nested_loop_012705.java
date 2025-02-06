b2f1eafe5577e64241a29b36c4dd16a5264b2b3e
Manasvi Kulkarni <manasvikulkarni@Manasvis-MacBook-Air.local>
2025-02-06 10:51:32 -0800

    Easier nested loop code

---
+++ nested_loop_012705.java
@@ -0,0 +1,10 @@
+public class nested_loop_012705 {
+    public static void main (String args[]){
+        for (int i = 0; i < 4; i++){
+            for (int j = 0; j < 3; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
