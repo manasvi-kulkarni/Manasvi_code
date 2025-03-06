812f7fef47a1468ca41e62515746e8f805569c5e
Manasvi Kulkarni <manasvikulkarni@Manasvis-MacBook-Air.local>
2025-03-05 16:28:51 -0800

    Adding for loop file to repository

---
+++ for_loop2.java
@@ -0,0 +1,9 @@
+public class for_loop2 {
+    public static void main (String args[]){
+        int num = 0;
+        for(int i = 2; i<=50; i++){
+            num = num+i;
+        }
+        System.out.println ("The new number is " + num);
+    }
+}
